a021388566514a7716189731544f5cd37c9a068a
Heba-ET <hebashaher111@gmail.com>
2024-08-27 19:22:19 -0700

    15 commit

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+
+schedule::command('app:backup-database')->daily(); // here we add our command
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
